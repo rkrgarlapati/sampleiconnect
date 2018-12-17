86b1c2ec38145f497f20b6f7d93b23bb5e785cdb
Garlapati Ravi Kumar (MAS) <GA20054501@wipro.com>
2018-12-17 17:05:14 +0530

    first commit

---
+++ src/main/java/com/roche/iConnect/IConnectApplication.java
@@ -0,0 +1,14 @@
+package com.roche.iConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IConnectApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/roche/iConnect/services/PartyService.java
@@ -0,0 +1,16 @@
+package com.roche.iConnect.services;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.ws.Response;
+import java.util.Date;
+
+@RestController
+public class PartyService {
+    @RequestMapping("/party")
+    public ResponseEntity party(){
+        return ResponseEntity.ok("Got from party service..."+new Date());
+    }
+}
